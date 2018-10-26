25576045769da2f1f63222bf2779ecd7bbcf8364
Pearson Buck <pearsonhbuck@gmail.com>
2018-10-25 20:56:20 -0700

    Connected a PostgreSQL server to the Node server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,56 @@
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
+
 const app = express();
 const port = process.env.PORT || 3000;
-let ideas = [];
+
+const dbConnectionString = '';
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/ideas', (req, res) => {
-  res.json(ideas); 
+  const database = new pg.Client(dbConnectionString);
+  const getQuery = 'SELECT * FROM ideas';
+
+  database.connect((err) => {
+    if (err) {
+      console.log(err);
+    }
+    database.query(getQuery, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(result.rows);
+        res.json(result.rows);
+        database.end();
+      }
+    });
+  });
 });
 
 app.post('/ideas', (req, res) => {
-  let idea = {
-    idea: req.body.idea,
-    category: req.body.category
-  };
-  ideas.push(idea);
+  const database = new pg.Client(dbConnectionString);
+  const idea = String(req.body.idea);
+  const category = req.body.category;
+  const postQuery = `INSERT INTO ideas(idea, category, postdate) VALUES(\'${idea}\', \'${category}\', current_date)`;
+  
+  database.connect((err) => {
+    if (err) {
+      console.log(err);
+    }
+    database.query(postQuery, (err) => {
+      if (err) {
+       console.log(err);
+      }
+      else {
+        database.end();
+      }
+    });
+  });
+
   res.send(idea);
 });
 
