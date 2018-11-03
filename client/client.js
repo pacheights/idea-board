01d55dd43d5d979ba3132e1b43a9fbbd2e88e31c
Pearson Buck <pearsonhbuck@gmail.com>
2018-11-02 23:09:49 -0700

    Edit event handler part 1

--- client/client.js
+++ client/client.js
@@ -4,7 +4,7 @@ const ideaForm = document.querySelector('.idea-form');
 const API_URL = 'http://localhost:3000/ideas';
 
 const listIdeas = () => {
-  clearIdeas();
+  // clearIdeas();
 
   fetch(API_URL)
     .then(res => res.json())
@@ -29,27 +29,45 @@ const clearIdeas = () => {
 const createIdeaElement = (idea) => {
   const container = document.createElement('div');
   const editButton = document.createElement('i');
+  const submitEditButton = document.createElement('i');
   const paragraph = document.createElement('p');
+  const editTextArea = document.createElement('textarea');
   const deleteButton = document.createElement('i');
 
   container.classList = 'single-idea';
   editButton.classList = 'edit-button fas fa-edit';
-  deleteButton.classList = 'delete-button fas fa-trash-alt';
+  submitEditButton.classList = 'submit-edit-button fas fa-check';
   paragraph.classList = 'idea-text';
+  editTextArea.classList = 'edit-idea-textarea';
+  deleteButton.classList = 'delete-button fas fa-trash-alt';
   
   paragraph.textContent = idea.idea;
   editButton.addEventListener('click', editButtonEventHandler)
   deleteButton.addEventListener('click', deleteButtonEventHandler)
 
   container.appendChild(editButton);
+  container.appendChild(submitEditButton);
   container.appendChild(paragraph);
+  container.appendChild(editTextArea);
   container.appendChild(deleteButton);
 
   return container;
 }
 
 const editButtonEventHandler = (event) => {
-  const password = prompt('Please enter the password to edit the idea');
+  // const password = prompt('Please enter the password to edit the idea');
+  const editButton = event.target.parentElement.children[0];
+  const submitEditButton = event.target.parentElement.children[1];
+  const ideaTextElement = event.target.parentElement.children[2];
+  const editTextArea = event.target.parentElement.children[3];
+  const ideaText = ideaTextElement.innerText;
+  const ideaTextNode = document.createTextNode(ideaText);
+  
+  editTextArea.appendChild(ideaTextNode);
+  editButton.style.fontSize = '0px';
+  editTextArea.style.display = 'block';
+  ideaTextElement.style.display = 'none';  
+  submitEditButton.style.fontSize = '20px';
 }
 
 const deleteButtonEventHandler = (event) => {
